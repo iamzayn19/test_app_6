96dbd4c9ad7a3571f5d3576ce622581dfef0a203
zain <iamzayn19@gmail.com>
2022-08-01 18:14:23 +0500

    add sign-up link

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,7 @@ class PagesController < ApplicationController
     
     def about
     end 
+
+    def sign_up
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   #root 'articles#index'
   root 'pages#home'
   get 'about', to: 'pages#about'
+  get 'sign_up', to: 'pages#sign_up'
 end
