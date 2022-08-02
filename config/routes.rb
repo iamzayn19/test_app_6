924c32042a23df4c05c4c6c2184348ebd3664835
zain <iamzayn19@gmail.com>
2022-08-02 10:06:31 +0500

    add sign_in sign_up routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   #root 'articles#index'
   root 'pages#home'
   get 'about', to: 'pages#about'
-  get 'sign_up', to: 'pages#sign_up'
+  get 'sign_up', to: 'users#new', as: :registration
+  post 'sign_in', to: 'users#sign_in'
 end
